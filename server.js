0b50c67ea4bb81443336479fe0c5fac7e8e9db49
Boi <you@example.com>
2018-09-22 09:06:45 +1200

    Added initial API basecode

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+// port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
+app.listen(6969, function () {
+    console.log('Max and Saksham have their beefy API on port: 6969. Created at: '+Date.now());
+});
+ 
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
+
+// test route
+app.get('/test', function (req, res) {
+
+    return res.json({"Message":"API IS SUCCESSFULLY HANDLING API REQUESTS"});
+});
  
 //--------- FOR LATER ;) -----------//
 // connection configurations
@@ -14,9 +36,3 @@ const mysql = require('mysql');
 // connect to database
 mc.connect();
 */
- 
-// default route
-app.get('/', function (req, res) {
-
-    return res.json({"Message":"API IS SUCCESSFULLY HANDLING API REQUESTS"});
-});
